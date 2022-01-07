1388c4329b7cc57ea740c0be107dd81b85aff763
Vinicius Braga <Vbragadev@gmail.com>
2022-01-07 00:14:56 -0300

    instalado python-decouple e configurado DEBUG

--- pythondjango/settings.py
+++ pythondjango/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+from decouple import config
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-@+(*d9d#kw8iky-9d12pmgzj$8^)omg)chu5ymskv4wsf2b+=y'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', cast=bool)
 
 ALLOWED_HOSTS = ['*']
 

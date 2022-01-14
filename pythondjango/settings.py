3d01b734976364cad0ae8559364bbd9898e2cb6f
Vinicius Braga <Vbragadev@gmail.com>
2022-01-13 22:58:08 -0300

    configured S3 bucket

--- pythondjango/settings.py
+++ pythondjango/settings.py
@@ -134,6 +134,38 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = 'media/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'mediafiles')
 
+AWS_ACESS_KEY_ID=config('AWS_ACESS_KEY_ID')
+
+#STORAGE CONFIGURATION IN S3 AWS
+if AWS_ACESS_KEY_ID:
+    AWS_SECRET_ACESS_KEY = config('AWS_SECRET_ACESS_KEY')
+    AWS_STORAGE_BUCKET_NAME = config('AWS_STORAGE_BUCKET_NAME')
+    AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400', }
+    AWS_PRELOAD_METADATA = True
+    AWS_AUTO_CREATE_BUCKET = False
+    AWS_QUERYSTRING_AUTH = False
+
+    AWS_DEFAULT_ACL = None
+
+     # static assets
+    STATICFILES_STORAGE = 's3_folder_storage.s3.StaticStorage'
+    STATIC_S3_PATH = 'static'
+    STATIC_ROOT = f'/{STATIC_S3_PATH}/'
+    STATIC_URL = f'//s3.amazonaws.com/{AWS_STORAGE_BUCKET_NAME}/{STATIC_S3_PATH}/'
+    ADMIN_MEDIA_PREFIX = STATIC_URL + 'admin/'
+
+    # Upload Media Folder
+    DEFAULT_FILE_STORAGE = 's3_folder_storage.s3.DefaultStorage'
+    DEFAULT_S3_PATH = 'media'
+    MEDIA_ROOT = f'/{DEFAULT_S3_PATH}/'
+    MEDIA_URL = f'//s3.amazonaws.com/{AWS_STORAGE_BUCKET_NAME}/{DEFAULT_S3_PATH}/'
+
+    INSTALLED_APPS.append('s3_folder_storage')
+    INSTALLED_APPS.append('storages')    
+
+
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 

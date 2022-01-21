2d6888e3b856b3c7437534069ca6e122559a8173
Vinicius Braga <Vbragadev@gmail.com>
2022-01-21 12:23:16 -0300

    update template

--- pythondjango/base/views.py
+++ pythondjango/base/views.py
@@ -4,4 +4,4 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
-    return HttpResponse('<html><body>Olá Django</body></html>')
+   return render(request, 'base/home.html')

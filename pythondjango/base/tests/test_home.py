6d4dce345f2528ee764dc4b868b23357e4ac9c2f
Vinicius Braga <Vbragadev@gmail.com>
2022-01-25 22:25:57 -0300

    create functions for test in django

--- pythondjango/base/tests/test_home.py
+++ pythondjango/base/tests/test_home.py
@@ -1,7 +1,12 @@
 from django.test import Client
-
+from pythondjango.django_assertions import assert_contains
 
 def test_status_code(client: Client):
     client = Client()
     resp = client.get('/')
     assert resp.status_code == 200
+
+def test_title(client: Client):
+    client = Client()
+    resp = client.get('/')
+    assert_contains(resp, '<title> Python Pro </title>')

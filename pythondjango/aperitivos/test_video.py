1a3703927604380b2a35ddb254f8066482aaefaf
Vinicius Braga <Vbragadev@gmail.com>
2022-02-07 23:41:37 -0300

    update poetry.lock

--- pythondjango/aperitivos/test_video.py
+++ pythondjango/aperitivos/test_video.py
@@ -3,7 +3,7 @@ from django.urls import reverse
 
 @pytest.fixture
 def resp(client):
-    return client.get(reverse('aperitivos:video', args=('motivacao,')))
+    return client.get(reverse('aperitivos:video', args=('motivacao',)))
     
 def test_status_code(resp):
     assert resp.status_code == 200    

581857645c7ca3507ad815a9d2194cd616cb9005
Vinicius Braga <Vbragadev@gmail.com>
2022-02-23 00:04:01 -0300

    add models.py on app aperitivos

--- pythondjango/aperitivos/views.py
+++ pythondjango/aperitivos/views.py
@@ -1,18 +1,21 @@
+from msilib.schema import Class
 from multiprocessing import context
+from urllib import response
 from django.shortcuts import render
+from aperitivos.models import Video
 
 
 videos = [
-        {'slug': 'motivacao', 'titulo': 'Video Aperitivo: Motivação', 'vimeo_id':675270281},
-        {'slug': 'instalacao-windows', 'titulo': 'Instalação-windows', 'vimeo_id':251497668},
-    ]
+        Video(slug='motivacao', titulo = 'Video Aperitivo: Motivação', vimeo_id='675270281'),
+        Video(slug='instalacao-windows', titulo = 'Instalação-windows', vimeo_id='251497668'),
+]
 
-videos_dct = {dct['slug']: dct for dct in videos}
+videos_dct = {v.slug: v for v in videos}
+
 def indice(request):
     return render(request, 'aperitivos/indice.html', context={'videos': videos})
 
 def video(request, slug):
     video = videos_dct[slug]
-
     return render(request, 'aperitivos/video.html', context={'video': video})
 

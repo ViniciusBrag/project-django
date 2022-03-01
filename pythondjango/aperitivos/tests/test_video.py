94d9975516d0a53b70d83a1ab79cd811e21145cf
Vinicius Braga <Vbragadev@gmail.com>
2022-03-01 17:36:11 -0300

    update project

--- pythondjango/aperitivos/tests/test_video.py
+++ pythondjango/aperitivos/tests/test_video.py
@@ -1,25 +1,38 @@
-from pytest_django import *
+from model_mommy import mommy
 import pytest
 from django.urls import reverse
+from pythondjango.aperitivos.models import Video
 from pythondjango.django_assertions import assert_contains
 
+@pytest.fixture
+def video(db):
+    return mommy.make(Video)
+    
 
 @pytest.fixture
-def resp(client, db):
-    print('resp')
-    return client.get(reverse('aperitivos:motivacao', args=('motivacao',)))
+def resp(client, video):
+    return client.get(reverse('aperitivos:video', args=(video.slug,)))
+
+@pytest.fixture
+def resp_video_nao_encontrado(client, video):
+    return client.get(reverse('aperitivos:video', args=(video.slug+'video_nao_existente',)))
 
     
 def test_status_code(resp):
     assert resp.status_code == 200 
+
+
+def test_status_code_video_nao_encontrado(resp_video_nao_encontrado):
+    assert resp_video_nao_encontrado == 404
+    
     
 
-def test_titulo_video(resp):
-    assert_contains(resp, '<h1>Video Aperitivo: Motivação</h1>')
+def test_titulo_video(resp, video):
+    assert_contains(resp, video.titulo)
 
 
-def test_conteudo_video(resp):
-    assert_contains(resp, '<div style="padding:59.41% 0 0 0;position:relative;"><iframe src="https://player.vimeo.com/video/675270281?h=2ede6adb1b&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen style="position:absolute;top:0;left:0;width:100%;height:100%;" title="y2metacom-Python Birds - Programa&amp;ccedil;&amp;atilde;o Orientada a Objetos-(480p)"></iframe></div><script src="https://player.vimeo.com/api/player.js"></script>')
+def test_conteudo_video(resp, video):
+    assert_contains(resp, f'<div style="padding:59.41% 0 0 0;position:relative;"><iframe src="https://player.vimeo.com/video/{video.vimeo_id}?h=2ede6adb1b&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen style="position:absolute;top:0;left:0;width:100%;height:100%;" title="y2metacom-Python Birds - Programa&amp;ccedil;&amp;atilde;o Orientada a Objetos-(480p)"></iframe></div><script src="https://player.vimeo.com/api/player.js"></script>"')
 
 
 

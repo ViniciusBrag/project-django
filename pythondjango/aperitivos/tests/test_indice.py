d1cd7f74b6f789e1f9fcfa3e6823d8dd3f039f54
Vinicius Braga <Vbragadev@gmail.com>
2022-03-01 17:36:11 -0300

    update project

--- pythondjango/aperitivos/tests/test_indice.py
+++ pythondjango/aperitivos/tests/test_indice.py
@@ -1,29 +1,35 @@
+from model_mommy import mommy
 import pytest
 from django.urls import reverse
+from pythondjango.aperitivos.models import Video
 from pythondjango.django_assertions import assert_contains
 
 
 @pytest.fixture
-def resp(client):
+def videos(db):
+    return mommy.make(Video, 3)
+
+@pytest.fixture
+def resp(client, videos):
     return client.get(reverse('aperitivos:indice'))
     
+
 def test_status_code(resp):
     assert resp.status_code == 200 
+    
 
-@pytest.mark.parametrize(
-    'titulo',
-    [
-       'Video Aperitivo: Motivação',
-       'Instalação-windows'
-    ]
-)
+def test_titulo_video(resp, videos):
+    for video in videos:
+        assert_contains(resp, video.titulo)
 
 
-def test_titulo_video(resp, titulo):
-    assert_contains(resp, titulo)
+def test_link_video(resp, videos):
+    for video in videos:
+        video_link = reverse('aperitivos:video', args=(video.slug,))
+        assert_contains(resp, f'href="{video_link}"')
 
 
-#def test_conteudo_video(resp):
- #   assert_contains(resp, '<div style="padding:59.41% 0 0 0;position:relative;"><iframe src="https://player.vimeo.com/video/675270281?h=2ede6adb1b&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen style="position:absolute;top:0;left:0;width:100%;height:100%;" title="y2metacom-Python Birds - Programa&amp;ccedil;&amp;atilde;o Orientada a Objetos-(480p)"></iframe></div><script src="https://player.vimeo.com/api/player.js"></script>')
+def test_conteudo_video(resp):
+    assert_contains(resp, '<div style="padding:59.41% 0 0 0;position:relative;"><iframe src="https://player.vimeo.com/video/675270281?h=2ede6adb1b&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen style="position:absolute;top:0;left:0;width:100%;height:100%;" title="y2metacom-Python Birds - Programa&amp;ccedil;&amp;atilde;o Orientada a Objetos-(480p)"></iframe></div><script src="https://player.vimeo.com/api/player.js"></script>')
 
 
--- pythondjango/aperitivos/views.py
+++ pythondjango/aperitivos/views.py
@@ -1,20 +1,14 @@
 from urllib import response
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from pythondjango.aperitivos.models import Video
 
 
-videos = [
-        Video(slug='motivacao', titulo = 'Video Aperitivo: Motivação', vimeo_id='675270281'),
-        Video(slug='instalacao-windows', titulo = 'Instalação-windows', vimeo_id='251497668'),
-]
-
-videos_dct = {v.slug: v for v in videos}
-
 def indice(request):
+    videos=Video.objects.order_by('creation').all()
     return render(request, 'aperitivos/indice.html', context={'videos': videos})
     
 
 def video(request, slug):
-    video = Video.objects.get(slug=slug)
+    video = get_object_or_404(Video, slug=slug)
     return render(request, 'aperitivos/video.html', context={'video': video})
 

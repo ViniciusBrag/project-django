470319bc53fbfb0387065c2f308398105010fc91
Vinicius Braga <Vbragadev@gmail.com>
2022-03-02 23:35:43 -0300

    update admin.py

--- pythondjango/aperitivos/admin.py
+++ pythondjango/aperitivos/admin.py
@@ -5,5 +5,5 @@ from pythondjango.aperitivos.models import Video
 @register(Video)
 class VideoAdmin(ModelAdmin):
     list_display = ('titulo', 'slug', 'creation', 'vimeo_id' )
-    orderimg = ('creation',)
+    ordering = ('creation',)
     prepopulated_fields = {'slug':('titulo',)}

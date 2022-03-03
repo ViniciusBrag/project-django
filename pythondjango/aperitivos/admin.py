6de25def54b4a6f6a58872799cb5e0f788ed502e
Vinicius Braga <Vbragadev@gmail.com>
2022-03-02 23:45:35 -0300

    add postgres in env-sample

--- pythondjango/aperitivos/admin.py
+++ pythondjango/aperitivos/admin.py
@@ -5,5 +5,4 @@ from pythondjango.aperitivos.models import Video
 @register(Video)
 class VideoAdmin(ModelAdmin):
     list_display = ('titulo', 'slug', 'creation', 'vimeo_id' )
-    ordering = ('creation',)
     prepopulated_fields = {'slug':('titulo',)}
